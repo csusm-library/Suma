8a1cf578f0e6eb5d803ca49407d362d563e2d0ba
Ian Chan <ichan@csusm.edu>
2013-03-08 15:05:19 -0800

    head css and script links

---
+++ service/views/scripts/includes/head.php
@@ -0,0 +1,23 @@
+<link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.1/css/bootstrap-combined.no-icons.min.css" rel="stylesheet">
+<link href="//netdna.bootstrapcdn.com/font-awesome/3.0.2/css/font-awesome.css" rel="stylesheet">
+<link href="/sumaserver/css/admin.css" rel="stylesheet">
+<link rel="stylesheet" type="text/css" href="<?php echo $baseDir; ?>/sumaserver/css/jquery-ui-theme/css/cupertino/jquery-ui-1.8.13.custom.css" />
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js" type="text/javascript"></script>
+<script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.8.21/jquery-ui.js" type="text/javascript"></script>
+<script src="<?php echo $baseDir; ?>/sumaserver/js/nestedSortable/nestedSortable.js" type="text/javascript"></script>
+<script src="<?php echo $baseDir; ?>/sumaserver/js/jquery.history.html4html5/jquery.history.js" type="text/javascript"></script>
+<script type="text/javascript">
+  WebFontConfig = {
+    google: { families: [ 'Tangerine', 'Cantarell' ] }
+  };
+  (function() {
+    var wf = document.createElement('script');
+    wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
+        '://ajax.googleapis.com/ajax/libs/webfont/1.1.2/webfont.js';
+    wf.type = 'text/javascript';
+    wf.async = 'true';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(wf, s);
+  })();
+</script>
+<script src="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.1/js/bootstrap.min.js"></script>
